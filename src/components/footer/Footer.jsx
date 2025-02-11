d3c4e90e5365be20fd6a830dd65afcd4a74e2520
Praveen Kumar <praveenshakya441@gmail.com>
2025-02-11 11:28:14 +0530

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,12 +6,12 @@ function Footer(){
     return(
         <div className="w-full h-90 flex flex-col items-center gap-10 py-10">
             <p className="font-source-sans-3 flex items-center gap-4">Praveen Kumar <span className="text-lg">©</span> {year}</p>
-            <div className="w-1/3 flex flex-row items-center justify-center gap-2">
+            <div className="w-1/3  md:w-1/4 flex flex-row items-center justify-center gap-2 md:gap-0">
             {
                 socialMediaLinks?.map((link, index)=>{
                     return(
                         <a href={link.href} key={index} target="_blank" rel='noopener noreferrer' className="w-full h-full cursor-pointer flex items-center justify-center">
-                            <link.icon className='w-4 h-4 text-blue  hover:scale-125 hover:transition-all duration-300' />
+                            <link.icon className='w-4 h-4 md:w-6 md:h-6 text-blue  hover:scale-125 hover:transition-all duration-300' />
                         </a>
                     )
                 })
